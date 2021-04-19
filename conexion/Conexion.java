6e3107731eb0a6e38b9353eb84bd99b302ad7f98
victorsanantonio <victorsanantonio@hotmail.com>
2021-04-19 19:50:03 +0200

    Commit final

--- conexion/Conexion.java
+++ conexion/Conexion.java
@@ -1,10 +1,14 @@
 package conexion;
 
-import com.mysql.jdbc.Connection;
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import org.apache.log4j.LogManager;
+
+import vista.VistaInicial;
 
 /**
  *
@@ -12,6 +16,7 @@ import java.util.logging.Logger;
  */
 public class Conexion {
 
+	static org.apache.log4j.Logger logger = LogManager.getLogger(Conexion.class);
     Connection connection = null;
 
     /**
@@ -22,14 +27,14 @@ public class Conexion {
     public Connection conexion() {
         try {
             //Declaraci贸n del driver JDBC
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.Driver");
             //Creaci贸n o instanciaci贸n de la conexi贸n
             connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/prueba_serbatic", "root", "");
             System.out.println("脡xito al conectar");
         } catch (SQLException e) {
             System.err.println("Error de conexi贸n " + e.getMessage());
         } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+            logger.error("Error de conexi髇");
         }
         return connection;
     }
--- vista/VistaInicial.java
+++ vista/VistaInicial.java
@@ -9,10 +9,17 @@ import controlador.Consultar;
 import controlador.Insertar;
 import controlador.LlamadasAFunciones;
 import interfaces.Infectable;
+
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Scanner;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 import modelo.Enfermero;
 import modelo.Paciente;
 import modelo.Persona;
@@ -22,183 +29,207 @@ import modelo.Persona;
  * @author admin
  */
 public class VistaInicial {
-    static Insertar insertar;
-    static Scanner sc = new Scanner(System.in);
+	private static Logger logger = LogManager.getLogger(VistaInicial.class);
+	static Insertar insertar;
+	static Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        mostrarPersonasSegunIdCiudad();
-        System.out.println("----PRUEBA PARA MOSTRAR INFECTABLES----");
-        mostrarInfectables();
-        System.out.println("---------------------------------------");
-        System.out.println("----PRUEBA PARA MOSTRAR VACUNABLES----");
-        mostrarVacunables();
-        System.out.println("---------------------------------------");
-        System.out.println("----PRUEBA PARA MOVEABLE----");
-        Paciente paciente = new Paciente();
-        paciente.visitarSuper();
-        System.out.println(paciente.getInfectado());
-        paciente.visitarTrabajo();
-        System.out.println(paciente.getInfectado());
-        paciente.cogerTransporte();
-        System.out.println(paciente.getInfectado());
-        System.out.println("---------------------------------------");
-        /*System.out.println("----EJECUTAR SIMULACIÓN DE DÍA----");
-        simularDia();
-        System.out.println("---------------------------------------");*/
-        System.out.println("----EJECUTAR MOVER----");
-        List<Paciente> pacientes = infectar();
-        System.out.println("---------------------------------------");
-        System.out.println("----PRUEBA DE INSERCIÓN DE DATOS----");
-        insertar = new Insertar();
-        insertar.start();
-        System.out.println("---------------------------------------");
-        
-        System.out.println("----PRUEBA DE LLAMADA A FUNCIÓN----");
-        LlamadasAFunciones llamadasAFunciones = new LlamadasAFunciones();
-        llamadasAFunciones.start();
-        System.out.println("---------------------------------------");
-    }
+	public static void main(String[] args) {
+		mostrarPersonasSegunIdCiudad();
+		System.out.println("----PRUEBA PARA MOSTRAR INFECTABLES----");
+		mostrarInfectables();
+		System.out.println("---------------------------------------");
+		System.out.println("----PRUEBA PARA MOSTRAR VACUNABLES----");
+		mostrarVacunables();
+		System.out.println("---------------------------------------");
+		System.out.println("----PRUEBA PARA MOVEABLE----");
+		Paciente paciente = new Paciente();
+		paciente.visitarSuper();
+		System.out.println(paciente.getInfectado());
+		paciente.visitarTrabajo();
+		System.out.println(paciente.getInfectado());
+		paciente.cogerTransporte();
+		System.out.println(paciente.getInfectado());
+		System.out.println("---------------------------------------");
+		/*
+		 * System.out.println("----EJECUTAR SIMULACIÃ“N DE DÃ�A----"); simularDia();
+		 * System.out.println("---------------------------------------");
+		 */
+		System.out.println("----EJECUTAR MOVER----");
+		List<Paciente> pacientes = infectar();
+		System.out.println("---------------------------------------");
+		System.out.println("----PRUEBA DE INSERCIÃ“N DE DATOS----");
+		insertar = new Insertar();
+		insertar.start();
+		System.out.println("---------------------------------------");
 
-    /**
-     * Método de simulación de infecciones en un día por desplazamientos. Este
-     * método ejecuta los movimientos de cada una de las personas infectables.
-     * En caso de que se haya infectado en alguno de los lugares, no se
-     * ejecutarán más métodos.
-     */
-    private static void simularDia() {
-        vacunarColeccionInfectados();
-        vacunarInfectables();
-    }
+		System.out.println("----PRUEBA DE LLAMADA A FUNCIÃ“N----");
+		LlamadasAFunciones llamadasAFunciones = new LlamadasAFunciones();
+		llamadasAFunciones.start();
+		System.out.println("---------------------------------------");
 
-    /**
-     * Obtener el último enfermero de la colección
-     */
-    private static Enfermero obtenerEnfermero(){
-        Enfermero vacunable = new Enfermero();
-        List<Enfermero> vacunables = vacunable.obtenerVacunables();
-        return vacunables.get(vacunables.size()-1);
-    }
-    
-    /**
-     * Vacunar a todas las personas infectables que hayan sido infectadas según una colección de infectados
-     */
-    private static void vacunarColeccionInfectados(){
-        //Recuperar la lista de las personas infectadas
-        //Collection<Infectable> infectados = infectar();
-        //Recuperar el enfermero para vacunar infectados
-        Enfermero enfermero = obtenerEnfermero();
-        //enfermero.vacunar(infectados);
-    }
-    
-    /**
-     * Vacunar a todas las personas infectables que hayan sido infectadas según una persona infectable
-     */
-    private static void vacunarInfectables(){
-        infectar();
-        Paciente paciente = new Paciente();
-        Infectable infectable = paciente;
-        //Recuperar el enfermero para vacunar infectados
-        Enfermero enfermero = obtenerEnfermero();
-        List<Paciente> pacientes = infectable.obtenerInfectables();
-        for(int i=0; i<pacientes.size(); i+=5){
-            System.out.println(pacientes.get(i).getInfectado());
-            enfermero.vacunar(pacientes.get(i+=2));
-            System.out.println(pacientes.get(i).getInfectado());
-        }
-    }
-    
-    /**
-     * Aplicar probabilidad de infección a todas las personas infectables que se desplacen
-     * @return Devolver todas las personas infectadas
-     */
-    private static List<Paciente> infectar() {
-        Paciente infectable = new Paciente();
-        List<Paciente> infectables = infectable.obtenerInfectables();
-        List<Paciente> infectados = new ArrayList<>();
-        for (int i = 0; i < infectables.size(); i++) {
+		// BasicConfigurator.configure();
+		ClassLoader loader = Thread.currentThread().getContextClassLoader();
+		URL url = loader.getResource("log4j.properties");
+		PropertyConfigurator.configure(url);
+		System.out.println("Hola");
+		logger.info("Este es el fichero de configuración: " + url);
+		logger.warn("Esto es un aviso");
+		// logger2.debug("Esto es un debug");
+	}
 
-            //Desplazar hasta el supermercado
-            infectables.get(i).visitarSuper();
-            //¿Se ha infectado?
-            if (infectables.get(i).getInfectado() == 1) {
-                //SÍ --> Mostrar mensaje
-                System.out.println(infectables.get(i).getNombre() + " Infectado al visitar el SUPERMERCADO: " + infectables.get(i).getInfectado());
-                infectados.add(infectables.get(i));
-            }
-            //NO-->
+	/**
+	 * MÃ©todo de simulaciÃ³n de infecciones en un dÃ­a por desplazamientos. Este
+	 * mÃ©todo ejecuta los movimientos de cada una de las personas infectables. En
+	 * caso de que se haya infectado en alguno de los lugares, no se ejecutarÃ¡n
+	 * mÃ¡s mÃ©todos.
+	 */
+	private static void simularDia() {
+		vacunarColeccionInfectados();
+		vacunarInfectables();
+	}
 
-            //Desplazar hasta el trabajo
-            infectables.get(i).visitarTrabajo();
-            //¿Se ha infectado?
-            if (infectables.get(i).getInfectado() == 1) {
-                //SÍ --> Mostrar mensaje
-                System.out.println(infectables.get(i).getNombre() + " Infectado al visitar el TRABAJO: " + infectables.get(i).getInfectado());
-                infectados.add(infectables.get(i));
-            }
-            //NO-->
+	/**
+	 * Obtener el Ãºltimo enfermero de la colecciÃ³n
+	 */
+	private static Enfermero obtenerEnfermero() {
+		Enfermero vacunable = new Enfermero();
+		List<Enfermero> vacunables = vacunable.obtenerVacunables();
+		return vacunables.get(vacunables.size() - 1);
+	}
 
-            //Coger el transporte público
-            infectables.get(i).cogerTransporte();
-            //¿Se ha infectado?
-            if (infectables.get(i).getInfectado() == 1) {
-                //SÍ --> Mostrar mensaje
-                System.out.println(infectables.get(i).getNombre() + " Infectado al coger el TRANSPORTE PÚBLICO: " + infectables.get(i).getInfectado());
-                infectados.add(infectables.get(i));
-            }
-        }
-        return infectados;
-    }
+	/**
+	 * Vacunar a todas las personas infectables que hayan sido infectadas segÃºn una
+	 * colecciÃ³n de infectados
+	 */
+	private static void vacunarColeccionInfectados() {
+		// Recuperar la lista de las personas infectadas
+		// Collection<Infectable> infectados = infectar();
+		// Recuperar el enfermero para vacunar infectados
+		Enfermero enfermero = obtenerEnfermero();
+		// enfermero.vacunar(infectados);
+	}
 
-    private static void mostrarInfectables() {
-        Paciente paciente = new Paciente();
-        List<Paciente> pacientes = paciente.obtenerInfectables();
-        for (Paciente p : pacientes) {
-            System.out.println(p);
-        }
-    }
+	/**
+	 * Vacunar a todas las personas infectables que hayan sido infectadas segÃºn una
+	 * persona infectable
+	 */
+	private static void vacunarInfectables() {
+		infectar();
+		Paciente paciente = new Paciente();
+		Infectable infectable = paciente;
+		// Recuperar el enfermero para vacunar infectados
+		Enfermero enfermero = obtenerEnfermero();
+		List<Paciente> pacientes = infectable.obtenerInfectables();
+		for (int i = 0; i < pacientes.size(); i += 5) {
+			System.out.println(pacientes.get(i).getInfectado());
+			enfermero.vacunar(pacientes.get(i += 2));
+			System.out.println(pacientes.get(i).getInfectado());
+			logger.info("Listado de pacientes");
+		}
+	}
 
-    private static void mostrarVacunables() {
-        Enfermero enfermero = new Enfermero();
-        List<Enfermero> enfermeros = enfermero.obtenerVacunables();
-        for (Enfermero e : enfermeros) {
-            System.out.println(e);
-        }
-    }
+	/**
+	 * Aplicar probabilidad de infecciÃ³n a todas las personas infectables que se
+	 * desplacen
+	 * 
+	 * @return Devolver todas las personas infectadas
+	 */
+	private static List<Paciente> infectar() {
+		Paciente infectable = new Paciente();
+		List<Paciente> infectables = infectable.obtenerInfectables();
+		List<Paciente> infectados = new ArrayList<>();
+		for (int i = 0; i < infectables.size(); i++) {
 
-    private static void mostrarPersonasSegunIdCiudad() {
-        Consultar consultar = new Consultar();
-        int idCiudad = solicitarCiudad();
-        System.out.println(idCiudad);
-        List<Persona> personas = consultar.consultarPersonasSegunIdCiudad(idCiudad);
+			// Desplazar hasta el supermercado
+			infectables.get(i).visitarSuper();
+			// Â¿Se ha infectado?
+			if (infectables.get(i).getInfectado() == 1) {
+				// SÃ� --> Mostrar mensaje
+				System.out.println(infectables.get(i).getNombre() + " Infectado al visitar el SUPERMERCADO: "
+						+ infectables.get(i).getInfectado());
+				infectados.add(infectables.get(i));
+				logger.info("Paciente añadido");
+			}
+			// NO-->
 
-        for (Persona p : personas) {
-            System.out.println(p);
-        }
-    }
+			// Desplazar hasta el trabajo
+			infectables.get(i).visitarTrabajo();
+			// Â¿Se ha infectado?
+			if (infectables.get(i).getInfectado() == 1) {
+				// SÃ� --> Mostrar mensaje
+				System.out.println(infectables.get(i).getNombre() + " Infectado al visitar el TRABAJO: "
+						+ infectables.get(i).getInfectado());
+				infectados.add(infectables.get(i));
+				logger.info("Paciente añadido");
+			}
+			// NO-->
 
-    /**
-     * Solicita una ciudad por teclado
-     */
-    private static int solicitarCiudad() {
-        System.out.print("Introduce el identificador de una ciudad:\t");
-        int idCiudad = leerInt();
-        return idCiudad;
-    }
+			// Coger el transporte pÃºblico
+			infectables.get(i).cogerTransporte();
+			// Â¿Se ha infectado?
+			if (infectables.get(i).getInfectado() == 1) {
+				// SÃ� --> Mostrar mensaje
+				System.out.println(infectables.get(i).getNombre() + " Infectado al coger el TRANSPORTE PÃšBLICO: "
+						+ infectables.get(i).getInfectado());
+				infectados.add(infectables.get(i));
+				logger.info("Paciente añadido");
+			}
+		}
+		return infectados;
+	}
 
-    /**
-     * Lee el entero de manera segura sin que se produzca la excepción de
-     * formato numérico
-     *
-     * @return
-     */
-    private static int leerInt() {
-        try {
-            String enteroCad = sc.nextLine();
-            int entero = Integer.parseInt(enteroCad);
-            return entero;
-        } catch (NumberFormatException nfe) {
-            System.err.println("Error de formato numérico. Debes de introducir un número entero");
-        }
-        return -1;
-    }
+	private static void mostrarInfectables() {
+		Paciente paciente = new Paciente();
+		List<Paciente> pacientes = paciente.obtenerInfectables();
+		for (Paciente p : pacientes) {
+			System.out.println(p);
+		}
+	}
+
+	private static void mostrarVacunables() {
+		Enfermero enfermero = new Enfermero();
+		List<Enfermero> enfermeros = enfermero.obtenerVacunables();
+		for (Enfermero e : enfermeros) {
+			System.out.println(e);
+		}
+	}
+
+	private static void mostrarPersonasSegunIdCiudad() {
+		Consultar consultar = new Consultar();
+		int idCiudad = solicitarCiudad();
+		System.out.println(idCiudad);
+		List<Persona> personas = consultar.consultarPersonasSegunIdCiudad(idCiudad);
+
+		for (Persona p : personas) {
+			System.out.println(p);
+		}
+	}
+
+	/**
+	 * Solicita una ciudad por teclado
+	 */
+	private static int solicitarCiudad() {
+		System.out.print("Introduce el identificador de una ciudad:\t");
+		int idCiudad = leerInt();
+		return idCiudad;
+	}
+
+	/**
+	 * Lee el entero de manera segura sin que se produzca la excepciÃ³n de formato
+	 * numÃ©rico
+	 *
+	 * @return
+	 */
+	private static int leerInt() {
+		try {
+			String enteroCad = sc.nextLine();
+			int entero = Integer.parseInt(enteroCad);
+			logger.info("Entero leído con éxito");
+			return entero;
+		} catch (NumberFormatException nfe) {
+			System.err.println("Error de formato numÃ©rico. Debes de introducir un nÃºmero entero");
+			logger.fatal("Error fatal de formato numérico");
+		}
+		return -1;
+	}
 }
